8c5b1604256cc4de254fa841e443b0b196042364
Ritesh Mahour <com  git config --global user.name Ritesh>
2026-04-21 13:40:45 +0530

    code pushed

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ const express = require("express");
 const app = express();
 
 app.use(express.json());
-app.get("/", (req, res) => {
+app.get("/", (_, res) => {
   res.send("Server is running.");
 });
 
